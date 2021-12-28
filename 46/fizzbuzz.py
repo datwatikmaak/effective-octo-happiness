4923055e3ce0cad85a0c2790c72e074659ed3987
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-12-28 07:47:38 +0100

    submission Bite 46 @ codechalleng.es

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,9 @@
 def fizzbuzz(num):
-    pass
+    if num % 5 == 0 and num % 3 == 0:
+        return "Fizz Buzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
