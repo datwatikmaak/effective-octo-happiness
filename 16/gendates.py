27a458067342cdbfef291d730f3b42f55a399065
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-12-28 10:35:06 +0100

    submission Bite 16 @ codechalleng.es

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,4 +4,9 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    pass
+    date_list = []
+    curr_date = PYBITES_BORN + timedelta(days=100)
+    for _ in range(1, 11):
+        date_list.append(curr_date)
+        curr_date += timedelta(days=100)
+    return date_list
