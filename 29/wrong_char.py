ce3b38f95fe833ceb4bd6b637b3d74b1507c36fb
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-02 11:22:00 +0100

    submission Bite 29 @ codechalleng.es

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,17 @@
+import collections
+import string
+
+LETTERS = list(string.ascii_lowercase + string.ascii_uppercase)
+NUMBERS = list(range(0, 10))
+
+
 def get_index_different_char(chars):
-    pass
+    lst = []
+    for c in chars:
+        if c not in LETTERS and c not in NUMBERS:
+            lst.append(0)
+        else:
+            lst.append(1)
+
+    least_common = collections.Counter(lst).most_common()[-1][0]
+    return [i for i, x in enumerate(lst) if x == least_common][0]
