b18ab3f079d3327d0766d20f1520b530c556d8c2
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-03 11:29:05 +0100

    submission Bite 39 @ codechalleng.es

--- 39/coursetime.py
+++ 39/coursetime.py
@@ -25,10 +25,21 @@ def get_all_timestamps():
 
         Return a list of MM:SS timestamps
     """
-    pass
+    with open(COURSE_TIMES, "r") as f:
+        content = f.read()
+        times = re.findall(r"\((\d{1,2}:\d{1,2})\)", content)
+
+    return times
 
 
 def calc_total_course_duration(timestamps):
     """Takes timestamps list as returned by get_all_timestamps
        and calculates the total duration as HH:MM:SS"""
-    pass
+    timestamps = get_all_timestamps()
+    time_sum = timedelta()
+    for i in timestamps:
+        (m, s) = i.split(":")
+        d = timedelta(minutes=int(m), seconds=int(s))
+        time_sum += d
+
+    return str(time_sum)
