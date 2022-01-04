8e9c3f396b47a6f27b26d00ce859e71f346339df
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-04 10:03:32 +0100

    submission Bite 51 @ codechalleng.es

--- 51/miller.py
+++ 51/miller.py
@@ -8,10 +8,10 @@ BITE_CREATED_DT = datetime.strptime('2018-02-26 23:24:04', '%Y-%m-%d %H:%M:%S')
 def py2_earth_hours_left(start_date=BITE_CREATED_DT):
     """Return how many hours, rounded to 2 decimals, Python 2 has
        left on Planet Earth (calculated from start_date)"""
-    pass
+    return round((((PY2_DEATH_DT - start_date).total_seconds()) / 3600), 2)  # 16152.6
 
 
 def py2_miller_min_left(start_date=BITE_CREATED_DT):
     """Return how many minutes, rounded to 2 decimals, Python 2 has
        left on Planet Miller (calculated from start_date)"""
-    pass
+    return round(((py2_earth_hours_left(start_date) / (7 * 365 * 24)) * 60), 2)
