fa463a5f272352ca1a7ea3e92bde125b68f04e28
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-08 09:49:00 +0100

    submission Bite 279 @ codechalleng.es

--- 279/armstrong.py
+++ 279/armstrong.py
@@ -1,2 +1,5 @@
 def is_armstrong(n: int) -> bool:
-    # your code ...
+    number_of_digits = len(str(n))
+    num_list = [int(i) for i in str(n)]
+    total = sum((i ** number_of_digits) for i in num_list)
+    return n < 10 or n == total
