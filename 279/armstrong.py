c06426a30bad1b27f5fa6e92df25bbc0754c39ec
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-08 09:48:01 +0100

    Revert "submission Bite 270 @ codechalleng.es"
    This reverts commit 30babf68ae5b33932811a0080a05632fa9248e98.

--- 279/armstrong.py
+++ 279/armstrong.py
@@ -1,5 +1,2 @@
 def is_armstrong(n: int) -> bool:
-    number_of_digits = len(str(n))
-    num_list = [int(i) for i in str(n)]
-    total = sum((i ** number_of_digits) for i in num_list)
-    return n < 10 or n == total
+    # your code ...
