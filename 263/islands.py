10213c32e2bcd5da4d08179bf8d486d3390c99ee
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-01-10 05:20:16 +0100

    submission Bite 263 @ codechalleng.es

--- 263/islands.py
+++ 263/islands.py
@@ -5,12 +5,15 @@ def count_islands(grid):
     Notes: island is denoted by 1, ocean by 0 islands is counted by continuously
         connected vertically or horizontally  by '1's.
     It's also preferred to check/mark the visited islands:
-    - eg. using the helper function - mark_islands().
+    - e.g. using the helper function - mark_islands().
     """
-    # islands = 0         # var. for the counts
-    # .....some operations.....
-    # mark_islands(r, c, grid)
-    # return islands
+    islands = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                islands += 1
+                mark_islands(i, j, grid)
+    return islands
 
 
 def mark_islands(i, j, grid):
@@ -18,4 +21,15 @@ def mark_islands(i, j, grid):
     Input: the row, column and grid
     Output: None. Just mark the visited islands as in-place operation.
     """
-    # grid[i][j] = '#'      # one way to mark visited ones - suggestion.
+    if (
+            (i >= 0)
+            and (i < len(grid))
+            and (j >= 0)
+            and (j < len(grid[i]))
+            and grid[i][j] == 1
+    ):
+        grid[i][j] = '#'
+        mark_islands(i - 1, j, grid)
+        mark_islands(i + 1, j, grid)
+        mark_islands(i, j - 1, grid)
+        mark_islands(i, j + 1, grid)
