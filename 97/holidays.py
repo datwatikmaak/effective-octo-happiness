6aa10436e990c544858f3380b2c6bde1409ca10b
Leonieke <datwatikmaak@gmail.com>
2022-01-16 10:25:44 +0100

    submission Bite 97 @ codechalleng.es

--- 97/holidays.py
+++ 97/holidays.py
@@ -4,7 +4,6 @@ from urllib.request import urlretrieve
 
 from bs4 import BeautifulSoup
 
-
 # prep data
 tmp = os.getenv("TMP", "/tmp")
 page = 'us_holidays.html'
@@ -24,4 +23,12 @@ def get_us_bank_holidays(content=content):
     """Receive scraped html output, make a BS object, parse the bank
        holiday table (css class = list-table), and return a dict of
        keys -> months and values -> list of bank holidays"""
-    pass
+    soup = BeautifulSoup(content, "html.parser")
+    rows = soup.select(".list-table tbody tr")
+    for row in rows:
+        cells = row.select("td")
+        holiday_month = cells[1].select_one("time").get("datetime")[5:7]
+        holiday_name = cells[3].getText().strip()
+        holidays[holiday_month].append(holiday_name)
+
+    return holidays
