b8979226fc2d58209ef2340a157aaa4709dc01bc
Leonieke <datwatikmaak@gmail.com>
2022-01-22 13:28:27 +0100

    submission Bite 54 @ codechalleng.es

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,33 @@
+import textwrap
+from pprint import pprint
+
 INDENTS = 4
+
+rosetti_unformatted = """
+                      Remember me when I am gone away,
+                      Gone far away into the silent land;
+                      When you can no more hold me by the hand,
+
+                      Nor I half turn to go yet turning stay.
+
+                      Remember me when no more day by day
+                      You tell me of our future that you planned:
+                      Only remember me; you understand
+                      """
 
 
 def print_hanging_indents(poem):
-    pass
+    indent = False
+    for line in poem.splitlines():
+        stripped_line = line.strip()
+        if len(stripped_line) == 0:
+            indent = False
+            continue
+        if indent:
+            print(f"{' ' * INDENTS}{stripped_line}")
+        else:
+            print(f"{stripped_line}")
+            indent = True
+
+
+print_hanging_indents(rosetti_unformatted)
