ae19485eb82453004d4651a5f011afde9cc6a26c
Leonieke <datwatikmaak@gmail.com>
2022-01-25 07:02:23 +0100

    submission Bite 86 @ codechalleng.es

--- 86/rgb2hex.py
+++ 86/rgb2hex.py
@@ -2,4 +2,8 @@ def rgb_to_hex(rgb):
     """Receives (r, g, b)  tuple, checks if each rgb int is within RGB
        boundaries (0, 255) and returns its converted hex, for example:
        Silver: input tuple = (192,192,192) -> output hex str = #C0C0C0"""
-    pass
+    for c in rgb:
+        if c < 0 or c > 255:
+            raise ValueError
+
+    return f"#{''.join(f'{hex(c)[2:].upper():0>2}' for c in rgb)}"
