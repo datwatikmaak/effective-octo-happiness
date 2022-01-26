d430ee1ce748a010f0f4c53eb567b28f07da7e73
Leonieke <datwatikmaak@gmail.com>
2022-01-26 06:43:05 +0100

    submission Bite 122 @ codechalleng.es

--- 122/anagram.py
+++ 122/anagram.py
@@ -2,4 +2,5 @@ def is_anagram(word1, word2):
     """Receives two words and returns True/False (boolean) if word2 is
        an anagram of word1, ignore case and spacing.
        About anagrams: https://en.wikipedia.org/wiki/Anagram"""
-    pass
+
+    return sorted("".join(word1.split()).lower()) == sorted("".join(word2.split()).lower())
