56d8aeae9713afc1c58f8a255d2501922ef7dd18
Leonieke <datwatikmaak@gmail.com>
2022-01-29 06:27:53 +0100

    submission Bite 157 @ codechalleng.es

--- 157/accents.py
+++ 157/accents.py
@@ -2,4 +2,13 @@ def filter_accents(text):
     """Return a sequence of accented characters found in
        the passed in lowercased text string
     """
-    pass
+    accents = []
+    for char in text.lower():
+        if len(char) == len(char.encode()):
+            continue
+        if char in accents:
+            continue
+        else:
+            accents.append(char)
+
+    return sorted(accents)
