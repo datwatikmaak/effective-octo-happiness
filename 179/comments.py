d4f94c0112c506b01977e529270b6a1b434a4def
Leonieke <datwatikmaak@gmail.com>
2022-02-07 06:46:44 +0100

    submission Bite 179 @ codechalleng.es

--- 179/comments.py
+++ 179/comments.py
@@ -1,3 +1,9 @@
+import re
+
+
 def strip_comments(code):
     # see Bite description
-    pass
+    docstring = re.compile(r'\s*"{3}.*?"{3}', re.DOTALL)
+    output = docstring.sub(r'', code)
+    comments = re.compile(r'\s*#\s.*')
+    return comments.sub(r'', output)
