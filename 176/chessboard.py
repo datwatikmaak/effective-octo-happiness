6127df6621ffcde93a28c0859dd01db389bfb679
Leonieke <datwatikmaak@gmail.com>
2022-02-13 05:54:44 +0100

    submission Bite 176 @ codechalleng.es

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,10 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    for i in range(size):
+        for _ in range(int(size / 2)):
+            if i % 2 == 0:
+                print(f"{WHITE}{BLACK}", end="")
+            else:
+                print(f"{BLACK}{WHITE}", end="")
+        print()
