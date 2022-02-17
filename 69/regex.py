ca96a31595610d77f0c59dc2b58ff5c4857f42b0
Leonieke <datwatikmaak@gmail.com>
2022-02-17 11:46:17 +0100

    submission Bite 69 @ codechalleng.es

--- 69/regex.py
+++ 69/regex.py
@@ -1,47 +1,59 @@
 import re
+from string import punctuation
 
 
 def has_timestamp(text):
     """Return True if text has a timestamp of this format:
        2014-07-03T23:30:37"""
-    pass
+    regex = r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}"
+    return bool(re.search(regex, text))
 
 
 def is_integer(number):
     """Return True if number is an integer"""
-    pass
+    regex = r"^(-?[\d])$"
+    return bool(re.search(regex, str(number)))
 
 
 def has_word_with_dashes(text):
     """Returns True if text has one or more words with dashes"""
-    pass
+    regex = r"[\d|\w]-[\d|\w]"
+    return bool(re.search(regex, text))
 
 
 def remove_all_parenthesis_words(text):
-    """Return text but without any words or phrases in parenthesis:
+    """Return text but without any words or phrases in parentheses:
        'Good morning (afternoon)' -> 'Good morning' (so don't forget
        leading spaces)"""
-    pass
+    regex = r"\s\((\w+|\d+.\d)\)"
+    return re.sub(regex, "", text)
 
 
 def split_string_on_punctuation(text):
     """Split on ?!.,; - e.g. "hi, how are you doing? blabla" ->
        ['hi', 'how are you doing', 'blabla']
        (make sure you strip trailing spaces)"""
-    pass
+    regex = r"[^\w\s]"
+    splitted = re.split(regex, text)
+    return [x.lstrip() for x in splitted if x]
 
 
 def remove_duplicate_spacing(text):
     """Replace multiple spaces by one space"""
-    pass
+    return re.sub(r"\s{2,}", " ", text)
 
 
 def has_three_consecutive_vowels(word):
     """Returns True if word has at least 3 consecutive vowels"""
-    pass
+    regex = r"[aeiou]{3}"
+    return bool(re.search(regex, word))
 
 
 def convert_emea_date_to_amer_date(date):
     """Convert dd/mm/yyyy (EMEA date format) to mm/dd/yyyy
        (AMER date format)"""
-    pass
+    regex = re.compile(r"(\d+/)(\d+/)(\d+)")
+    return regex.sub(r"\2\1\3", date)
+
+
+print(convert_emea_date_to_amer_date("31/03/2018"))
