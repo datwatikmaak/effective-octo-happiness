b4f15b76a4785c1459ed21452d899e5dc4b3ef5e
Leonieke <datwatikmaak@gmail.com>
2022-02-24 05:47:21 +0100

    submission Bite 224 @ codechalleng.es

--- 224/split.py
+++ 224/split.py
@@ -1,4 +1,8 @@
+import re
+
+
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    pass
+    sentences = re.findall(r"([A-Z].+?[.?!])\s(?=[A-Z]|$)", text, re.DOTALL)
+    return [sentence.replace("\n", " ") for sentence in sentences]
