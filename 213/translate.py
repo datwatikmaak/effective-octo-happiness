395c453d76f303c1b75da97352462fa218bd5372
Leonieke <datwatikmaak@gmail.com>
2022-02-24 06:09:04 +0100

    submission Bite 213 @ codechalleng.es

--- 213/translate.py
+++ 213/translate.py
@@ -6,4 +6,17 @@ def fix_translation(org_text, trans_text):
        Parse trans_text restoring the original (English) code (wrapped inside
        code and pre tags) into it. Return the fixed translation str
     """
-    pass
+    org_text_code = re.findall(r"<code[^>]*>(.+?\n*.+?)</code>", org_text, re.MULTILINE | re.DOTALL)
+    org_text_pre = re.findall(r"<pre[^>]*>(.+?\n*.+?)</pre>", org_text, re.MULTILINE | re.DOTALL)
+    trans_text_code = re.findall(r"<code[^>]*>(.+?\n*.+?)</code>", trans_text, re.MULTILINE | re.DOTALL)
+    trans_text_pre = re.findall(r"<pre[^>]*>(.+?\n*.+?)</pre>", trans_text, re.MULTILINE | re.DOTALL)
+
+    if org_text_code:
+        for i, word in enumerate(org_text_code):
+            trans_text = trans_text.replace(trans_text_code[i], word)
+
+    if org_text_pre:
+        for i, word in enumerate(org_text_pre):
+            trans_text = trans_text.replace(trans_text_pre[i], word)
+
+    return trans_text
