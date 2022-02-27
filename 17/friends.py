2196716ad85621d9736c05e9a96d01e3daa709eb
Leonieke <datwatikmaak@gmail.com>
2022-02-27 10:19:36 +0100

    submission Bite 17 @ codechalleng.es

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,8 @@
-def friends_teams():
-    pass
+from itertools import combinations, permutations
+
+
+def friends_teams(friends_list: list, team_size: int = 2, order_does_matter: bool = False):
+    if order_does_matter:
+        return permutations(friends_list, team_size)
+    else:
+        return combinations(friends_list, team_size)
