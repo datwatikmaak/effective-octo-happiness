f1cf849fe2701ebd0e8da345dc0784a72fbae9f0
Leonieke <datwatikmaak@gmail.com>
2022-02-27 09:42:30 +0100

    submission Bite 66 @ codechalleng.es

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -2,4 +2,11 @@ def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+    result = []
+    total = 0
+
+    for i, s in enumerate(sequence, 1):
+        total += s
+        result.append(round(total / i, 2))
+
+    return result
