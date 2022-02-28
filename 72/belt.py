57662cca1ee374df452a5766c49a4d21adf36db8
Leonieke <datwatikmaak@gmail.com>
2022-02-28 09:37:18 +0100

    submission Bite 72 @ codechalleng.es

--- 72/belt.py
+++ 72/belt.py
@@ -1,6 +1,14 @@
+from collections import OrderedDict
+from itertools import filterfalse
+
 scores = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
 belts = 'white yellow orange green blue brown black paneled red'.split()
 
+ninjas = OrderedDict(zip(scores, belts))
+minimum_score, maximum_score = min(scores), max(scores)
 
-def get_belt(user_score, scores=scores, belts=belts):
-    pass
+
+def get_belt(user_score):
+    if user_score < minimum_score:
+        return None
+    return ninjas[list(filterfalse(lambda x: x > user_score, ninjas.keys()))[-1]]
