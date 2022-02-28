ba67d816d17f336aa9a69add5623983b3987ccce
Leonieke <datwatikmaak@gmail.com>
2022-02-28 09:41:41 +0100

    submission Bite 99 @ codechalleng.es

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,2 +1,8 @@
+from string import ascii_uppercase
+
+
 def sequence_generator():
-    pass
+    while True:
+        for char in enumerate(ascii_uppercase, start=1):
+            yield char[0]
+            yield char[1]
