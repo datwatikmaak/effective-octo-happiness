2c77d91a38e933c3fdd53f86edb101e46c1adac9
Leonieke <datwatikmaak@gmail.com>
2022-03-01 09:08:15 +0100

    submission Bite 171 @ codechalleng.es

--- 171/animation.py
+++ 171/animation.py
@@ -10,7 +10,13 @@ def spinner(seconds):
     """Make a terminal loader/spinner animation using the imports above.
        Takes seconds argument = time for the spinner to run.
        Does not return anything, only prints to stdout."""
-    pass
+    time_end = time() + seconds
+    spin = cycle(SPINNER_STATES)
+    for s in spin:
+        print(f"{s}\r", end="", flush=True)
+        sleep(STATE_TRANSITION_TIME)
+        if time() > time_end:
+            break
 
 
 if __name__ == '__main__':
